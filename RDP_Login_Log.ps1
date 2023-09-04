19c02ccfdf53226584642bcc15a2773e638fbf59
wkapdlzkz <143983432+wkapdlzkz@users.noreply.github.com>
2023-09-04 14:00:11 +0900

    Update RDP_Login_Log.ps1

--- RDP_Login_Log.ps1
+++ RDP_Login_Log.ps1
@@ -12,6 +12,7 @@ $rdpEvents = Get-WinEvent -FilterHashtable @{Logname='Security'; ID=4624, 4625} 
     }
 
 # 접속 시도한 IP 주소들을 그룹화하여 성공, 실패 횟수와 시도 횟수를 계산합니다.
+# Group the IP addresses that attempted to access to calculate success, failure, and number of attempts.
 $ipCounts = $rdpEvents | Group-Object -Property IPAddress | ForEach-Object {
     $attempts = $_.Group.Count
     $successes = ($_.Group | Where-Object { $_.Status -eq "Success" }).Count
@@ -25,7 +26,8 @@ $ipCounts = $rdpEvents | Group-Object -Property IPAddress | ForEach-Object {
 } | Sort-Object -Property AttemptCount -Descending
 
 # 결과를 txt 파일로 저장
-$logFolder = "C:\Users\jslee97\Desktop\Logs"
+# Save results as a txt file
+$logFolder = "C:\Users\Desktop\Logs"
 $logFileName = "$(Get-Date -Format 'yyyy-MM-dd-HH-mm-ss').txt"
 $logFile = Join-Path $logFolder $logFileName
 $ipCounts | Format-Table -AutoSize | Out-File $logFile
